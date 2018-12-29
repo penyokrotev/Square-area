cd702cbd46fcaf0cc1ab38682cbca9e5eb08fc15
penyokrotev <45831577+penyokrotev@users.noreply.github.com>
2018-12-29 12:06:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_area
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			int area = (number * number);
+			Console.WriteLine(area);
+		}
+	}
+}
